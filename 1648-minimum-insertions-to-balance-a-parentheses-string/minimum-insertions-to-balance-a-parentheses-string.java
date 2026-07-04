87b023ec6a92a0e7166332c69ea1d1aab032bb12
Gaurav Chandak <gauravchandakk8@gmail.com>
2026-07-04 17:58:18 +0530

    Time: 26 ms (37.29%) | Memory: 47.8 MB (11.04%) - LeetSync

--- 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
@@ -2,23 +2,25 @@ class Solution {
     public int minInsertions(String s) {
         Stack<Character> st=new Stack<>();
         int count=0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='(' ){
+        char[] arr=s.toCharArray();
+        int j=arr.length;
+        for(int i=0;i<j;i++){
+            if(arr[i]=='(' ){
                 st.push('(');
                 
             }
             
-                else if(i+1<s.length()&& s.charAt(i)==')' && s.charAt(i+1)==')' && st.isEmpty()){
+                else if(i+1<j&& arr[i]==')' && arr[i+1]==')' && st.isEmpty()){
                     count+=1;
                     i+=1;
                 }
-                else if(i+1<s.length()&& s.charAt(i)==')'&& s.charAt(i+1)==')'){
+                else if(i+1<j&& arr[i]==')'&& arr[i+1]==')'){
                     st.pop();
                     i+=1;
                 }
                 
             
-            else if( st.isEmpty() &&s.charAt(i)==')'){
+            else if( st.isEmpty() &&arr[i]==')'){
                 count+=2;
             }
             else{
