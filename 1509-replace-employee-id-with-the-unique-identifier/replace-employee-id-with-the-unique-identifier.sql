b7a2420ad1e86aad79ab9755fe97f4b79df903c5
Gaurav Chandak <gauravchandakk8@gmail.com>
2026-08-02 00:34:42 +0530

    Time: 1167 ms (81.11%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select case when e.id=u.id then u.unique_id else null end as unique_id , name
+select unique_id , name
 from Employees as e
 left join EmployeeUNI as u
 on e.id=u.id;
