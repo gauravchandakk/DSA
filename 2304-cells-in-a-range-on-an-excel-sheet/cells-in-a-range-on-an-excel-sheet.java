119ef6260e7c190e937cd04147672fb859e70a64
Gaurav Chandak <gauravchandakk8@gmail.com>
2026-08-10 23:28:17 +0530

    Time: 4 ms (70.13%) | Memory: 52.6 MB (29.19%) - LeetSync

--- 2304-cells-in-a-range-on-an-excel-sheet/cells-in-a-range-on-an-excel-sheet.java
+++ 2304-cells-in-a-range-on-an-excel-sheet/cells-in-a-range-on-an-excel-sheet.java
@@ -1,20 +1,17 @@
 class Solution {
     public List<String> cellsInRange(String s) {
         List<String> list=new ArrayList<>();
-        StringBuilder a=new StringBuilder(s.substring(0,2));
-        list.add(a.toString());
-        while(!a.toString().equals(s.substring(3,5))){
-            
+        String a=s.substring(0,2);
+        list.add(a);
+        while(!a.equals(s.substring(3,5))){
             if(a.charAt(1)==s.charAt(4)){
-                a.setCharAt(0,(char)(a.charAt(0)+1));
-                a.setCharAt(1,s.charAt(1));
+                a=""+(char)(a.charAt(0)+1)+s.charAt(1);
             }
             else{
-                
-                a.setCharAt(1,(char)(a.charAt(1)+1));
+                a=""+a.charAt(0)+(char)(a.charAt(1)+1);
 
             }
-            list.add(a.toString());
+            list.add(a);
         }
         return list;
     }
